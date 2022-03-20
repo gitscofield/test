d3c73d05c2054d99c9168e038b9f56936f884208
sco <scofield.five@gmail.com>
2022-03-20 12:05:21 +0800

    start

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Tinder/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Tinder  from './components/vue-tinder/Tinder.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Tinder
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import Tinder  from './vue-tinder'
+
+Vue.config.productionTip = false
+
+// console.log(Tinder)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
+
